655677cd03ee20d0b093b922c94c2b846860cae2
Felipa Mendez <felipalouise@gmail.com>
2022-01-10 19:58:56 -0600

    commit tree 73d45c02f5f58e1e106d6efabf7bdae631b1d9c6

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(value){
+  let result
+  if (value  <= 400) {
+    result = "This one is on me!"
+  } else if (value > 2500) {
+    result = 'No can do.'
+  } else if (value > 2000) {
+  result = 'I will gladly take your thirty bucks.'}
+  return result
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+scuberGreetingForFeet(199);
+
+function ternaryCheckCity(city) {
+  return city === 'NYC' ? "Ok, sounds good." : "No go.";
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+ternaryCheckCity('Pittsburgh');
+
+
+
+function switchOnCharmFromTip(tip) {
+  switch(tip) {
+    case 'generous':
+    return "Thank you so much."
+    case 'not as generous':
+    return 'Thank you.'
+    default:
+    return 'Bye.'
+  }
 }
+
+switchOnCharmFromTip('generous');
